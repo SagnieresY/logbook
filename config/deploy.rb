5c97af1fcf79c77b88970ff2513807699e1ab2d8
Yves Sagnieres <sagnieres.yves@gmail.com>
2019-04-11 02:05:25 -0400

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :application, "logbook"
 set :repo_url, "git@github.com:sagnieresy/logbook.git"
 
 # Deploy to the user's home directory
-set :deploy_to, "/home/deploy/#{fetch :application}"
+set :deploy_to, "/home/yves/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
@@ -12,7 +12,7 @@ set :keep_releases, 5
 
 # Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
 # This is useful if you don't want to use ENV variables
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
