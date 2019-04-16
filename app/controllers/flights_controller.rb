00595b9ee44e2c6953bed3226b76eff440e10e59
Yves Sagnieres <sagnieres.yves@gmail.com>
2019-04-16 17:25:06 -0400

    first table done

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,32 +1,42 @@
 class FlightsController < ApplicationController
+
   def index
     @flights = Flight.all
   end
 
   def show
-    @flight = Flight.find(params[:id])
   end
 
-  #create
   def new
+    @flight = Flight.new
   end
 
   def create
+    flight = Flight.new(flight_params)
+    flight.save
+    redirect_to flights_path
   end
 
-  #update
   def edit
-     @restaurant = Restaurant.find(params[:id])
   end
 
   def update
-
+    @flight.update(flight_params)
+    redirect_to flight_path(@flight)
   end
-
 
   def destroy
-    @flight = Flight.find(params[:id])
     @flight.destroy
+    redirect_to flights_path
   end
 
+  private
+
+  def set_flight
+    @flight = Flight.find(params[:id])
+  end
+
+  def flight_params
+    params.require(:flight).permit(:date, :aircraft_type, :aircraft_registration, :pic_name, :copi_name, :engine_type, :day_time, :night_time, :simulator, :takeoffs_landings, :cross_country, :command_type, :from, :to)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'flights#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get "flights", to: "flights#index"
-  # get "flights/:id", to: "flights#show"
-
-  # #create
-  # get "flights/new", to: "flights#new"
-  # post "flights", to: "flights#create"
-
-  # #update
-  # get "flights/:id/edit", to: "flights#edit"
-  # patch "flights/:id", to: "flights#update"
-
-  # #delete
-  # delete "flights/:id", to: "flights#destroy"
+  resources :flights
 
 end
